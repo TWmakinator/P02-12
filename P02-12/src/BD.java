c285f027a6cfcb20c1dc5108e3a240ab2149c4d5
Victor <Victor@192.168.0.14>
2019-10-15 12:16:08 +0200

    Creación VentanaRegistro y conexión de BD

---
+++ P02-12/src/BD.java
@@ -0,0 +1,153 @@
+
+import java.sql.*;
+
+/** Clase de gestión de base de datos del examen
+ * Funciona con un hilo propio para no interrumpir el tiempo de ejecución
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+public class BD {
+	
+
+	private static Exception lastError = null;  // Información de último error SQL ocurrido
+	
+	/** Inicializa una BD SQLITE y devuelve una conexión con ella
+	 * @param nombreBD	Nombre de fichero de la base de datos
+	 * @return	Conexión con la base de datos indicada. Si hay algún error, se devuelve null
+	 */
+	public static Connection initBD( String nombreBD ) {
+		try {
+		    Class.forName("org.sqlite.JDBC");
+		    Connection con = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
+		    return con;
+		} catch (ClassNotFoundException | SQLException e) {
+			lastError = e;
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/** Devuelve statement para usar la base de datos
+	 * @param con	Conexión ya creada y abierta a la base de datos
+	 * @return	sentencia de trabajo si se crea correctamente, null si hay cualquier error
+	 */
+	public static Statement usarBD( Connection con ) {
+		try {
+			Statement statement = con.createStatement();
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			return statement;
+		} catch (SQLException e) {
+			lastError = e;
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/** Crea las tablas de la base de datos. Si ya existen, las deja tal cual
+	 * @param con	Conexión ya creada y abierta a la base de datos
+	 * @return	sentencia de trabajo si se crea correctamente, null si hay cualquier error
+	 */
+	public static Statement usarCrearTablasBD( Connection con ) {
+		try {
+			Statement statement = con.createStatement();
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			try {
+				statement.executeUpdate("create table usuario " +
+					"(nick string, clave string, admin int)");
+			} catch (SQLException e) {} // Tabla ya existe. Nada que hacer
+			return statement;
+		} catch (SQLException e) {
+			lastError = e;
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/** Reinicia en blanco las tablas de la base de datos. 
+	 * UTILIZAR ESTE MËTODO CON PRECAUCIÓN. Borra todos los datos que hubiera ya en las tablas
+	 * @param con	Conexión ya creada y abierta a la base de datos
+	 * @return	sentencia de trabajo si se borra correctamente, null si hay cualquier error
+	 */
+	public static Statement reiniciarBD( Connection con ) {
+		try {
+			Statement statement = con.createStatement();
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			statement.executeUpdate("drop table if exists usuario");
+			return usarCrearTablasBD( con );
+		} catch (SQLException e) {
+			lastError = e;
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/** Cierra la base de datos abierta y cierra el hilo de proceso de base de datos
+	 * @param con	Conexión abierta de la BD
+	 * @param st	Sentencia abierta de la BD
+	 */
+	public static void cerrarBD( final Connection con, final Statement st ) {
+			try {
+				if (st!=null) st.close();
+				if (con!=null) con.close();
+			} catch (SQLException e) {
+				lastError = e;
+				e.printStackTrace();
+			}
+	}
+	
+	/** Devuelve la información de excepción del último error producido por cualquiera 
+	 * de los métodos de gestión de base de datos
+	 */
+	public static Exception getLastError() {
+		return lastError;
+	}
+	
+	/*MÉTODOS DEL PROYECTO*/
+	
+	/**
+	 * Devuelve información sobre la existencia del usuario en la bbdd
+	 * @param nick
+	 * @param clave
+	 * @return  0: Si el nick es incorrecto
+	 *           1: Si el nick es correcto pero la clave no
+	 *           2: Si el nick y la clave son correctos
+	 
+	 */
+	
+	public static int buscarUsuario(String nick, String clave) {
+		int resultado = 0;
+		String query = "SELECT * FROM usuario WHERE nick='"+nick+"'";
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		try {
+			ResultSet rs = st.executeQuery(query);
+			if(rs.next()) {
+				String cl = rs.getString(1);
+				if(cl.equals(clave)){
+					resultado = 2;
+				}else {
+					resultado = 1;
+				}
+			}/*else {
+				resultado = 0;
+			}*/
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return resultado;
+	}
+	
+	public static void registrarUsuario(String nick, String clave) {
+		String sql = "INSERT INTO usuario VALUES('"+nick+"','"+clave+"')";
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		try {
+			st.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ P02-12/src/VentanaRegistro.java
@@ -0,0 +1,182 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class VentanaRegistro extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtNick;
+	private JTextField txtClave;
+	private JTextField txtNickR;
+	private JTextField txtClaveR;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaRegistro frame = new VentanaRegistro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaRegistro() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel pAbajo = new JPanel();
+		contentPane.add(pAbajo, BorderLayout.SOUTH);
+		
+		JButton btnAceptar = new JButton("ACEPTAR");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnAceptar.setForeground(Color.RED);
+		btnAceptar.setBackground(Color.BLACK);
+		pAbajo.add(btnAceptar);
+		
+		JPanel pArriba = new JPanel();
+		contentPane.add(pArriba, BorderLayout.NORTH);
+		
+		JLabel lblDeustogames = new JLabel("DeustoGames");
+		pArriba.add(lblDeustogames);
+		
+		JPanel pCentro = new JPanel();
+		contentPane.add(pCentro, BorderLayout.CENTER);
+		pCentro.setLayout(new GridLayout(1, 2, 0, 0));
+		
+		JPanel panel1 = new JPanel();
+		pCentro.add(panel1);
+		panel1.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnInicioSesin = new JButton("INICIO SESIÓN");
+		btnInicioSesin.setForeground(Color.DARK_GRAY);
+		btnInicioSesin.setBackground(Color.BLACK);
+		btnInicioSesin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int resultado = BD.buscarUsuario(txtNick.getText(), txtClave.getText());
+				if(resultado == 2) {
+					JOptionPane.showMessageDialog(null, "Inicio de sesión correcto", "INICIO SESIÓN", JOptionPane.INFORMATION_MESSAGE);
+				}else if (resultado == 1){
+					JOptionPane.showMessageDialog(null, "Clave incorrecta", "ERROR", JOptionPane.ERROR_MESSAGE);
+				}else {
+					JOptionPane.showMessageDialog(null, "Usuario desconocido", "ERROR", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		panel1.add(btnInicioSesin, BorderLayout.SOUTH);
+		
+		JPanel panel = new JPanel();
+		panel1.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNick = new JLabel("NICK:");
+		lblNick.setBounds(10, 32, 46, 14);
+		panel.add(lblNick);
+		
+		txtNick = new JTextField();
+		txtNick.setBounds(62, 29, 128, 20);
+		panel.add(txtNick);
+		txtNick.setColumns(10);
+		
+		JLabel lblClave = new JLabel("CLAVE");
+		lblClave.setBounds(10, 85, 46, 14);
+		panel.add(lblClave);
+		
+		txtClave = new JTextField();
+		txtClave.setBounds(62, 82, 128, 20);
+		panel.add(txtClave);
+		txtClave.setColumns(10);
+		
+		JPanel panel2 = new JPanel();
+		pCentro.add(panel2);
+		panel2.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnRegistro = new JButton("REGISTRO");
+		btnRegistro.setBackground(Color.BLACK);
+		btnRegistro.setForeground(Color.BLACK);
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/*
+				String correo="newton.marian@gmail.com";
+				int pos1 = correo.indexOf("@");
+				int pos2 = correo.lastIndexOf("@");
+				if(pos1==pos2 && pos1!=-1) {
+					//tiene una única arroba
+				}*/
+				
+				if(txtNickR.getText().equals("") || txtClaveR.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Hay que rellenar todos los campos", "ERROR", JOptionPane.ERROR_MESSAGE);
+				}else {
+					try {
+						
+							int resultado = BD.buscarUsuario(txtNickR.getText(), txtClaveR.getText());
+							if(resultado != 0) {
+								JOptionPane.showMessageDialog(null, "Ese usuario ya existe", "ERROR", JOptionPane.ERROR_MESSAGE);
+							}else {
+								BD.registrarUsuario(txtNickR.getText(), txtClaveR.getText());
+								JOptionPane.showMessageDialog(null, "Registro realizado con éxito", "REGISTRO", JOptionPane.INFORMATION_MESSAGE);
+							}
+						}
+					catch(NumberFormatException e1) {
+						JOptionPane.showMessageDialog(null, "La edad tiene que ser numérica", "ERROR", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		});
+		panel2.add(btnRegistro, BorderLayout.SOUTH);
+		
+		JPanel panel_1 = new JPanel();
+		panel2.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(null);
+		
+		JLabel lblNickR = new JLabel("NICK:");
+		lblNickR.setBounds(10, 33, 46, 14);
+		panel_1.add(lblNickR);
+		
+		txtNickR = new JTextField();
+		txtNickR.setColumns(10);
+		txtNickR.setBounds(62, 30, 128, 20);
+		panel_1.add(txtNickR);
+		
+		JLabel lblClaveR = new JLabel("CLAVE");
+		lblClaveR.setBounds(10, 86, 46, 14);
+		panel_1.add(lblClaveR);
+		
+		txtClaveR = new JTextField();
+		txtClaveR.setColumns(10);
+		txtClaveR.setBounds(62, 83, 128, 20);
+		panel_1.add(txtClaveR);
+	}
+}
