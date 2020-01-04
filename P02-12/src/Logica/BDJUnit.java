085d6cbcc4388f341a09a41df09e9f1cafbbf211
Victor <Victor@DESKTOP-04UAMA5>
2020-01-04 17:38:49 +0100

    Implementación de varios JUnit más (8 en total)

--- P02-12/src/Logica/BDJUnit.java
+++ P02-12/src/Logica/BDJUnit.java
@@ -19,8 +19,64 @@ public class BDJUnit {
 	assertEquals(2, enc);
 }
 
-
+	@Test
+	public void BuscarUsuario(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		int enc=BD.buscarUsuario("admin", "admin1");
+		assertEquals(2, enc);
 		
+	}
+		
+	@Test
+	public void BuscarNombreUsuario(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		int enc= BD.buscarNombreUsuario("alvaro");
+		assertEquals(0,enc);
+	}
+	
+	@Test
+	public void BuscarCarta(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		int enc= BD.buscarCarta("Ensartar a los críticos");
+		assertEquals(2,enc);
+		
+	}
+	
+	@Test
+	public void ObtenerPrecioCarta(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		float precio= BD.obtenerPrecioCarta("CartasMagic/Carta1.jpg");
+		assertEquals(6, precio, 0.0);
+	}
+	
+	@Test
+	public void ObtenerRutaFoto(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		String ruta= BD.obtenerRutaFoto(1);
+		assertEquals("CartasMagic/Carta1.jpg", ruta);
+	}
+	
+	@Test
+	public void ObtenerNombreCarta(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		String nombre= BD.obtenerNombreCarta("CartasMagic/Carta1.jpg");
+		assertEquals("Engendro del caos",nombre);
+	}
+	
+	@Test
+	public void ObtenerRarezaCarta(){
+		Connection con = BD.initBD("BaseDeDatos.db");
+		Statement st = BD.usarBD(con);
+		String rareza= BD.obtenerRarezaCarta("CartasMagic/Carta1.jpg");
+		assertEquals("Mítica",rareza);
+	}
+	
 	}
 
 
